006caadea2827961a6d78cc756c79eade030ad5c
Minhazur Rahman <rafi.rahman03@yahoo.com>
2022-04-09 00:58:17 +0600

    default error message created

---
+++ api/constant/errorMessages.js
@@ -0,0 +1,27 @@
+export const ERROR = {
+    USER_NOT_EXIST: "User Not Exist",
+    NOT_FOUND: "User Not Found",
+    TOKEN_NOT_EXIST: "Token Not Exist",
+    TOKEN_VERIFY_SUCCESSFULLY: "Token Verified Successfully",
+    TOKEN_NOT_VERIFIED: "Token is not verified",
+    TOKEN_CREATED_SUCCESSFULLY: "A token has been created and sent to email",
+    PASSWORD_MISMATCH: "Password didn't match",
+    PASSWORD_RESET: "PasswordReset",
+    PASSWORD_MATCHED: "Matched",
+    SUCCESSFUL: "Successful",
+    PROFILE_EDITED: "Profile edited",
+    FIRST_NAME_EMPTY: "First Name can't be empty",
+    LAST_NAME_EMPTY: "Last Name can't be empty",
+    SHORT_NAME: "Name is too short",
+    VALID_EMAIL: "Enter a valid email",
+    VALID_PASS: "Enter a valid password",
+    EMAIL_EXIST: "Email has already been registered",
+    EMAIL_VERIFIED: "Email is verified",
+    INVALID_PHONE_NUMBER: "Phone number is invalid",
+    IMAGE_UPLOAD_ERROR : "Image Upload Error",
+}
+
+export const ERROR_STATUS ={
+    BAD_INPUT:"BAD_INPUT",
+    UNAUTHORIZED:"UNAUTHORIZED"
+}
