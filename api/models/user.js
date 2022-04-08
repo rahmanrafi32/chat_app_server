fb23ee716e6a17618bd3527938b1a3b02cee8c5f
Minhazur Rahman <rafi.rahman03@yahoo.com>
2022-04-09 00:56:23 +0600

    user schema edited

--- api/models/user.js
+++ api/models/user.js
@@ -1,45 +1,25 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-
-const validateEmail = function (email) {
-    const regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return regEx.test(email)
-};
-
-const validatePass = function (password) {
-    const regEx = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/;
-    return regEx.test(password)
-};
 
 const userSchema = new mongoose.Schema({
         firstName: {
             type: String,
-            required: 'First name is required',
-            min: [2, 'Name is too short'],
+            trim: true,
         },
         lastName: {
             type: String,
-            required: 'Last name is required',
-            min: [2, 'Name is too short'],
+            trim: true,
         },
         phoneNumber: {
             type: String,
-            required: 'Phone number is required',
+            trim: true,
         },
         email: {
             type: String,
             trim: true,
             lowercase: true,
-            unique: true,
-            required: 'Email address is required',
-            validate: [validateEmail, 'Please enter a valid email address'],
-            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
         },
         password: {
             type: String,
-            required: 'Password is required',
-            validate: [validatePass, 'Please enter a valid password'],
-            match: [/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/, 'Please enter a valid password']
         },
         profilePicture: {
             type: String
