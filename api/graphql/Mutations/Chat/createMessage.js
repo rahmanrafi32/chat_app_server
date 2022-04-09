d0a50c8c511f75a41ef14e37469d2a2694533e40
Minhazur Rahman Rafi <rafi.rahman03@yahoo,com>
2022-04-09 13:01:31 +0600

    create message added

---
+++ api/graphql/Mutations/Chat/createMessage.js
@@ -0,0 +1,34 @@
+import {User} from "../../../models/user";
+import {Message} from "../../../models/message";
+// import {pubSub} from "../../../constant/pubSub";
+
+export const createMessage = async (parents, {
+    payload: {
+        text,
+        attachment,
+        receiverID,
+        conversationID
+    }
+}, {_id}, req) => {
+    try {
+        let receiver = await User.findById({_id: receiverID});
+
+        const newMessage = new Message({
+            text,
+            attachment,
+            sender: _id,
+            receiver: receiver._id,
+            conversationID
+        });
+
+        // await pubSub.publish("NEW_MESSAGE", {newMessage});
+
+        await newMessage.save();
+
+        return {
+            message: "Message sent"
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
